0c50fd39ea5c45c39b4c7dd7d97d12d00342740b
nuclearnic <nuclearnicdev@gmail.com>
2018-05-09 22:28:34 +0200

    complete activities 1 & 2

--- README.md
+++ README.md
@@ -1,7 +1,39 @@
-### Prep Activity: Learn to Move Sphero Programmatically
+## Prep Activity: Learn to Control Sphero Programmatically
 
-<i> Short overview of scope and purpose goes here </i>
+<img src="assets/sphero.jpg" width="120" height="100" align="left" />
 
-* Change Sphero's color and move him programmatically
+<br>
 
-* Sphero's initial position is randomized: Find Sphero's coordinates with the Odomoter and move him to <b>(0,0)</b>.
+We're going to be playing with a <b>Sphero</b> robot on Make Day! The two activities in this repo are intended to give you some familiarity with Sphero's Javascript SDK. Since you probably don't have a Sphero at home, we'll use a mock library that will give you the basic idea.
+
+<br>
+
+If you want explore the SDK itself then have a look here: https://github.com/orbotix/sphero.js
+
+<b>Cheat Sheet.</b> Below are the commands you'll be learning about:
+
+* `sphero.connect`: Connects to Sphero. All your code goes inside this function.
+
+* `sphero.roll`: Takes two arguments - distance (unitless) and direction (in degrees). The direction you specify is 0° relative to the x-axis - see the image below:
+<img src="assets/Counterclockwise_rotation.png" width="150" height="150" />
+
+* `sphero.setColor`: Takes a string with the color as argument. In lower-case, e.g. "blue", "red", "green", etc.
+
+* `sphero.getColor`: Returns Sphero's current color.
+
+* `sphero.readOdometer`: We've cheated a little here! In the Sphero SDK the Odometer streams data asynchronously when invoking `streamOdometer`, while here we're just returning Sphero's position. We've included this function so you can get a little familiar with the concept.
+
+<b>TIP:</b> See `example.js` for all of the commands required to finish the two activities.
+
+### Activity #1
+
+<i> Change Sphero's color and move him programmatically. </i>
+
+* Open activity_1.js and add the required code so that Sphero's final position is at <b>(0, 200)</b> and it's color is <b>"Blue"</b>.
+
+### Activity #2 (Optional)
+
+<i> Sphero's initial position is randomized: Find Sphero's coordinates with the Odomoter and move him to <b>(0, 0)</b>. Send Sphero home! </i>
+
+* Open `activity_2.js` and find Sphero's initial position.
+* You have to send Sphero back to <b>(0, 0)</b>, so use the Odometer's reading and figure out a way to do that!
--- sphero.js
+++ sphero.js
@@ -1,49 +1,62 @@
-// The sphero object below mimics *some* of the basic commands you have
-// access to when using the sphero.js library
+// The sphero object below mimics *some* of the basic commands
+// you have access to when using the sphero.js library
 
 module.exports =
 {
     // iniitial state
     xPos: 0,
     yPos: 0,
-    color: "red",
+    color: 'transparent',
     // mimic Sphero's connect method
     connect: function(work) {
-        console.log("let's roll!");
+        console.log("...let's roll!")
         work();
     },
     readOdometer: function () {
-        console.log("receiving sphero's coordinates from the odometer...");
-        return [this.xPos.toFixed(2), this.yPos.toFixed(2)];
+        console.log("-> receiving sphero's coordinates from the odometer...");
+        var x = parseFloat(this.xPos.toFixed(2));
+        var y = parseFloat(this.yPos.toFixed(2));
+        return [x,y];
     },
     getColor: function () {
         return this.color;
     },
     roll: function (distance, direction) {
-        console.log('moving sphero ' + distance + 'units in the ' + direction + 'direction...')
+        console.log('-> moving sphero ' + distance + 'units in the ' + direction + 'direction...')
         var rads = direction * ( Math.PI / 180 );
         this.xPos += distance * Math.cos(rads);
         this.yPos += distance * Math.sin(rads);
     },
     setColor: function (color) {
-        console.log("changing sphero's color to " + color + '...');
+        console.log("-> changing sphero's color to " + color + '...');
         this.color = color;
     },
     assertState: function (xPos, yPos, color) {
-        console.log('asserting that sphero is ' + color + ' and at' + '[' + xPos + ',' + yPos + ']')
+        var colorMatches = false
+        var posMatches = false
+        xPos = parseFloat(xPos);
+        yPos = parseFloat(yPos);
+
+        console.log('-> asserting that sphero is ' + color + ' and at ' + '[' + xPos + ',' + yPos + ']...')
+
         if (this.color == color) {
             console.log('color matches');
+            colorMatches = true;
+        } else if (color == undefined) {
+            console.log('not checking color');
+            colorMatches = true;
         } else {
             console.log("color doesn't match");
         }
         var position = this.readOdometer();
         if (position[0] == xPos.toFixed(2) && position[1] == yPos.toFixed(2)) {
             console.log('position matches');
+            posMatches = true;
         } else {
             console.log("position doesn't match");
             console.log('Odometer reading is: ' + position);
-            // console.log(xPos.toFixed(2) + ' ' +  yPos.toFixed(2))
             console.log(['expecting Sphero at: ' + xPos.toFixed(2), yPos.toFixed(2)]);
         }
+        return (colorMatches && posMatches);
     }
 };
