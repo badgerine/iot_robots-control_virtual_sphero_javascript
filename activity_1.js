3de4719cfd05a650fe959165f9848168fb9d4443
Sibusiso <sbu.matsini@gmail.com>
2018-06-13 01:33:58 +0200

    activity complete

--- activity_1.js
+++ activity_1.js
@@ -5,10 +5,11 @@ var sphero = require('./sphero.js');
 
 sphero.connect(function() {
   // your code goes here
-
+  sphero.roll(200,90);
+  sphero.setColor('blue');
 
   // let's check your code...
-  if (sphero.assertState(0, 200, 'blue')) {
+  if (sphero.assertState(0.00, 200.00, 'blue')) {
     console.log("\nCongrats, you've completed Activity #1!")
   } else {
     console.log("\nWoops! Sphero isn't quite there yet...");
