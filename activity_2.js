3d6b2211ebed6ad246b87d90b16e30e57d6e4384
Sibusiso <sbu.matsini@gmail.com>
2018-06-13 01:54:45 +0200

    activity 2 done

--- activity_2.js
+++ activity_2.js
@@ -5,8 +5,12 @@ var sphero = require('./sphero.js');
 
 sphero.connect_random(function() {
   // your code goes between here...
-
-
+  let position = sphero.readOdometer();
+  console.log("sphero intial position: "+position[0]+", "+position[1]);
+  sphero.roll(-(position[1]),90);
+  sphero.roll(-(position[0]),0);
+  position = sphero.readOdometer();
+  console.log("sphero position after roll: "+position[0]+", "+position[1]);
   // let's check your code...
   if (sphero.assertState(0, 0)) {
     console.log("\nCongrats, you've completed Activity #2!")
